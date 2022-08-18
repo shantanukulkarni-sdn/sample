a1d01234b7447eb57bc58a7df863d6adb0ba37f7
Shantanu Kulkarni <36811956+heyshantu13@users.noreply.github.com>
2022-08-18 13:46:35 +0530

    Add files via upload

---
+++ responseMSG.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    MESSAGE_RECEIVED_SUCCESS: "Message Received To Mirth Connect Service",
+    MESSAGE_RECEIVED_FAILED : "We are not able to process message yet.",
+    APPOINTMENT_FETCH_SUCCESS : "Appointment list fetch succesfully",
+    APPOINTMENT_FETCH_NOT_FOUND : "No Appointment Available yet",
+    SOMETHING_WENT_WRONG : "Something went wrong",
+
+}
